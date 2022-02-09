a40d3dd7342d681a21664e897befc866d815d6f3
unknown <edward62740@gmail.com>
2022-02-09 13:00:56 +0800

    Update

--- sink/GNU ARM v10.2.1 - Default/gecko_sdk_4.0.1/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.0.1/platform/service/system/src/sl_system_process_action.o: \
- C\:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_process_action.c \
- c\:\users\extr\ 1\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C\:\Users\EXTR\ 1\SimplicityStudio\v5_workspace\sink\autogen/sl_event_handler.h
-c\:\users\extr\ 1\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C\:\Users\EXTR\ 1\SimplicityStudio\v5_workspace\sink\autogen/sl_event_handler.h:
