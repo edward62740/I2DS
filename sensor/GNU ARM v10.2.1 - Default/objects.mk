b000543012b3b4bd3aaca94490fbf0e6210e572f
unknown <edward62740@gmail.com>
2022-03-19 18:19:26 +0800

    Update

--- sensor/GNU ARM v10.2.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-aes-security-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-frequency-hopping-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-packet-queue-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-parent-support-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-stack-common-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//protocol/flex/libs/libconnect-stack-counters-library-efr32xg2x-gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_config_zgm230sb27hgn_gcc.a C:/Users/EXTR\ 1/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg23_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
