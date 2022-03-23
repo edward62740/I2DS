65b60eb1ee9cb022474d617c305733076d16537d
unknown <edward62740@gmail.com>
2022-03-23 17:03:33 +0800

    Update

--- PIRSN/app_init.c
+++ PIRSN/app_init.c
@@ -41,6 +41,7 @@
 #include "em_iadc.h"
 #include "em_cmu.h"
 #include "em_emu.h"
+#include "em_prs.h"
 // -----------------------------------------------------------------------------
 //                              Macros and Typedefs
 // -----------------------------------------------------------------------------
@@ -104,58 +105,45 @@ app_log_info("Network status 0x%02X\n", status);
     * requests.  Deleting such lines is safe on xG21 devices and will
     * reduce provide a small reduction in code size.
     */
+
+   CMU_ClockEnable(cmuClock_PRS, true);
+
+    // Connect the specified PRS channel to the LETIMER producer
+    PRS_SourceAsyncSignalSet(0,
+                             PRS_ASYNC_CH_CTRL_SOURCESEL_MODEM,
+                             PRS_MODEMH_PRESENT);
+
+    // Connect the specified PRS channel to the IADC as the consumer
+    PRS_ConnectConsumer(0,
+                        prsTypeAsync,
+                        prsConsumerIADC0_SINGLETRIGGER);
+
    CMU_ClockEnable(cmuClock_IADC0, true);
-   CMU_ClockEnable(cmuClock_GPIO, true);
 
-   // Use the FSRC0 as the IADC clock so it can run in EM2
-   CMU_ClockSelectSet(cmuClock_IADCCLK, cmuSelect_FSRCO);
 
-   // Set the prescaler needed for the intended IADC clock frequency
-   init.srcClkPrescale = IADC_calcSrcClkPrescale(IADC0, 20000000, 0);
 
-   // Shutdown between conversions to reduce current
-   init.warmup = iadcWarmupNormal;
 
-   /*
-    * Configuration 0 is used by both scan and single conversions by
-    * default.  Use unbuffered AVDD as reference and specify the
-    * AVDD supply voltage in mV.
-    *
-    * Resolution is not configurable directly but is based on the
-    * selected oversampling ratio (osrHighSpeed), which defaults to
-    * 2x and generates 12-bit results.
-    */
-   initAllConfigs.configs[0].reference = iadcCfgReferenceVddx;
-   initAllConfigs.configs[0].vRef = 3300;
+   initAllConfigs.configs[0].reference = iadcCfgReferenceInt1V2;
+   initAllConfigs.configs[0].vRef = 1200;
    initAllConfigs.configs[0].osrHighSpeed = iadcCfgOsrHighSpeed2x;
 
-   /*
-    * CLK_SRC_ADC must be prescaled by some value greater than 1 to
-    * derive the intended CLK_ADC frequency.
-    *
-    * Based on the default 2x oversampling rate (OSRHS)...
-    *
-    * conversion time = ((4 * OSRHS) + 2) / fCLK_ADC
-    *
-    * ...which results in a maximum sampling rate of 833 ksps with the
-    * 2-clock input multiplexer switching time is included.
-    */
    initAllConfigs.configs[0].adcClkPrescale = IADC_calcAdcClkPrescale(IADC0,
                                                                       1000000,
                                                                       0,
                                                                       iadcCfgModeNormal,
                                                                       init.srcClkPrescale);
 
-   /*
-    * Specify the input channel.  When negInput = iadcNegInputGnd, the
-    * conversion is single-ended.
-    */
+
+   initSingle.triggerSelect = iadcTriggerSelPrs0PosEdge;
+   initSingle.dataValidLevel = iadcFifoCfgDvl4;
+   initSingle.start = true;
+
+
    singleInput.posInput   = iadcPosInputAvdd;
    singleInput.negInput   = iadcNegInputGnd;
 
    // Initialize IADC
    IADC_init(IADC0, &init, &initAllConfigs);
-
    // Initialize a single-channel conversion
    IADC_initSingle(IADC0, &initSingle, &singleInput);
 
@@ -168,6 +156,7 @@ app_log_info("Network status 0x%02X\n", status);
    // Enable IADC interrupts
    NVIC_ClearPendingIRQ(IADC_IRQn);
    NVIC_EnableIRQ(IADC_IRQn);
+
 
   sl_power_manager_remove_em_requirement(SL_POWER_MANAGER_EM1);
        sl_power_manager_add_em_requirement(SL_POWER_MANAGER_EM2);
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Internal Intruder Detection System (I<sup>2</sup>DS)
 
 This is a sub-GHz proprietary security sensor system. 
+ 
+# Current progress
+## CPN
+* read voltage rx
 
+## PIRSN
+* iadc trig measurement on prs signal from radio
+* 
 
+## ACSN
+nil
