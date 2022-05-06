332ee23f5d1b095e0fc83567bdf4d6a0b86966a5
unknown <edward62740@gmail.com>
2022-05-06 17:43:26 +0800

    clean

--- PIRSN/GNU ARM v10.2.1 - Default/gecko_sdk_4.0.1/platform/service/iostream/src/sl_iostream_stdlib_config.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.0.1/platform/service/iostream/src/sl_iostream_stdlib_config.o: \
- C\:/Users/█████████/SimplicityStudio/SDKs/gecko_sdk/platform/service/iostream/src/sl_iostream_stdlib_config.c \
- c\:\users\workstation\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h
-c\:\users\workstation\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
