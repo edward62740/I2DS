845fb16fd83169f799860671493f0b9b27100487
unknown <edward62740@gmail.com>
2022-05-06 17:43:26 +0800

    clean

--- PIRSN/GNU ARM v10.2.1 - Default/gecko_sdk_4.0.1/platform/service/system/src/sl_system_init.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.0.1/platform/service/system/src/sl_system_init.o: \
- C\:/Users/█████████/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_init.c \
- c\:\users\workstation\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C\:\Users\█████████\Desktop\I2DS\PIRSN\autogen/sl_event_handler.h
-c\:\users\workstation\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C\:\Users\█████████\Desktop\I2DS\PIRSN\autogen/sl_event_handler.h:
